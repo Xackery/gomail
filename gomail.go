4600c9a29457965fbf11f1f923c6461181c0e9ef
Xackery Xtal <xackery@gmail.com>
2015-09-28 20:58:54 -0700

    Initial commit.

---
+++ gomail.go
@@ -0,0 +1,34 @@
+package gomail
+
+import (
+	"errors"
+	"net/smtp"
+)
+
+func Send(email string, password string, to []string, subject string, content string) (err error) {
+	if len(to) < 1 {
+		errors.New("No recipient specified")
+		return
+	}
+	hostname := "smtp.gmail.com"
+	auth := smtp.PlainAuth(
+		"",
+		email,
+		password,
+		hostname,
+	)
+
+	msg := []byte("To: " + to[0] + "\r\n" +
+		"Subject: " + subject + "\r\n" +
+		"\r\n" +
+		content)
+
+	err = smtp.SendMail(
+		hostname+":587",
+		auth,
+		email,
+		to,
+		msg,
+	)
+	return
+}
